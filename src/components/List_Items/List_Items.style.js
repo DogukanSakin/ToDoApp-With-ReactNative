49d1d97758ac893b6a8f944d61e6f24cc37dfdbc
DogukanSakin <dogukansakin1@hotmail.com>
2022-04-08 00:23:18 +0300

    08.04.2022

---
+++ src/components/List_Items/List_Items.style.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+export default StyleSheet.create({
+    container:{
+        backgroundColor:'#4f9c40',
+        borderRadius:10,
+        marginTop:10
+    },
+    itemText:{
+        color:'white',
+        fontSize:18,
+        padding:10
+    },
+    completedContainer:{
+        backgroundColor: '#696969',
+        borderRadius:10,
+        marginTop:10
+    },
+    completedText:{
+        color: 'white',
+        textDecorationLine: 'line-through',
+        fontSize:18,
+        padding:10
+    }
+
+ 
+
+
+
+
+})
